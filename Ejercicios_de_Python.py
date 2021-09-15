8283f5a924daeae09af61d88d47e99a59e7c60f9
Alejo <alejozuritar@gmail.com>
2021-09-14 22:28:20 -0300

    Creacion del programa de ejercicios de Python

---
+++ Ejercicios_de_Python.py
@@ -0,0 +1,34 @@
+#Ejercicio de calculadora v 1.0.0
+primero = input('Ingrese el primer numero: ')
+
+try: 
+    primero = int(primero)
+except:
+    primero = 'Incorrecto'
+
+if primero == 'Incorrecto': 
+    print('Ingresaste mal un dato, prueba de nuevo solo con numeros')
+    exit()
+
+segundo = input('Ingrese el segundo numero: ')
+
+try: 
+    segundo = int(segundo)
+except:
+    segundo = 'Incorrecto'
+
+if segundo == 'Incorrecto':
+    print('Ingresaste mal un dato, prueba de nuevo solo con numeros')
+    exit()
+
+simbolo = input('Ingrese operacion: ')
+if simbolo == '+':
+    print('Suma: ', primero + segundo)
+elif simbolo == '-':
+    print('Resta: ', primero - segundo)
+elif simbolo == '*':
+    print('Multiplicacion: ', primero * segundo)
+elif simbolo == '/':
+    print('Divison: ', primero / segundo)
+else:
+    print('El simbolo ingresado no es valido')
